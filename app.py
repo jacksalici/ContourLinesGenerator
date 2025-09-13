7e51ca7d7ec49e471fe445dc50837e7e0951166d
Giacomo Salici <giambo2000@gmail.com>
2025-09-14 00:22:23 +0200

    canvas class

--- app.py
+++ app.py
@@ -1,35 +1,32 @@
 
 import svg
 from geometry import *
+from canvas import *
 from utils import *
 
 DEPTH = 10
-WIDTH = 400
-HEIGHT = 400
 
-
-
-def draw(points: Points) -> svg.SVG:
-    elements: list[svg.Element] = []
+if __name__ == '__main__':
     
-    for x, y, z in points.get_all(scale=[WIDTH, HEIGHT]):
+    points = Points.generate(10, randomize=True, seed=14)
+    canvas = Canvas()
+    
+    for x, y, z in points.get_all(scale=canvas.size):
         
-        elements.append(svg.Circle(
-                cx=x, cy=y, r=4,
+        canvas.add(svg.Circle(
+                cx=x, cy=y, r=10,
                 fill="black",
                 fill_opacity=z
             ))
         
-    return svg.SVG(
-        viewBox=svg.ViewBoxSpec(0, 0, WIDTH, HEIGHT),
-        elements=elements,
-    )
-
-
-if __name__ == '__main__':
+    for line in [Line(points.get_all()[i], points.get_all()[i+1]) for i in range(len(points)-1)]:
+        for x, y, z in line.getInterpolatedPoints(15).get_all(scale=canvas.size):
+            canvas.add(svg.Circle(
+                cx=x, cy=y, r=2,
+                fill="red",
+                fill_opacity=z
+            ))
     
-    points = Points.generate(10, randomize=True)
-    file = draw(points)
-    export(file, "test.svg")
+    canvas.export()
     
     
